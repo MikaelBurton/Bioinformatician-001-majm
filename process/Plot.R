29f1a0286eb1f30c25a0b97c48ecc59d79f94cbf
MikaelBurton <majb_93@hotmail.com>
2018-03-06 00:46:09 -0600

    Create Plot.R

---
+++ process/Plot.R
@@ -0,0 +1,4 @@
+x <- c(3.70732, 4.27473, 2.47423, 53.1429, 72.6667)
+y <- c(82.8283, 91.9192, 65.1007, 101.449, 97.9899)
+
+plot(y,x, xlab="Mean Depth of Coverage (X)", ylab="Breadth of Coverage(%)", main="Sequencing Coverage", ylim=c(0,110), xlim=c(0,100), pch=15, col="blue")
